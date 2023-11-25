91aba89daf85b3a8a6b93da79d95434e9069d9e9
Ravi Madhavan <ravimadhavan@Ravis-MacBook-Pro.local>
2023-11-25 15:59:02 -0700

    DB url changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
-/*module.exports = {
-    url: "mongodb+srv://admin:UelLHsSFHrr3KXWU@fastfindanycluster.6nbtvwp.mongodb.net/customer_service?retryWrites=true&w=majority"
-  };*/
-
 module.exports = {
+    url: "mongodb+srv://admin:UelLHsSFHrr3KXWU@fastfindanycluster.6nbtvwp.mongodb.net/customer_service?retryWrites=true&w=majority"
+  };
+
+/*module.exports = {
   url: "mongodb://localhost:27017/customer_service"
-};
+};*/
